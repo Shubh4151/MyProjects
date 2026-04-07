2b8f70d9c7b1e62e43e90d5dd8ffcc4f02f3bd9f
Shubh4151 <agaleshubhangi20@gmail.com>
2026-04-07 14:42:45 +0530

    2

--- ResultPortal/Backend/index.js
+++ ResultPortal/Backend/index.js
@@ -6,10 +6,11 @@ const rt = require("./routes/rt")
 mongoose.connect("mongodb://127.0.0.1:27017/s34resportal").then(()=>{
     console.log("ok")
 })
-let app=express()
+let app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({"extended":true}))
 app.use(cors())
 app.use("/pic",express.static("./phots"))
 app.use("/",rt)
+
 app.listen(5000)
